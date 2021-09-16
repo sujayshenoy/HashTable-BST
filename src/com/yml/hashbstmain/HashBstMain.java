f55567f5f7f6755288ca24a11d18d9ef7ec5d55f
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-17 02:07:13 +0530

    Method added to search elements in the binary search tree

--- src/com/yml/hashbstmain/HashBstMain.java
+++ src/com/yml/hashbstmain/HashBstMain.java
@@ -9,8 +9,15 @@ public class HashBstMain {
         bst.add(56);
         bst.add(30);
         bst.add(70);
+        bst.add(63);
         System.out.println("Size of the tree : "+bst.size());
-
         bst.print();
+
+        if (bst.search(63)) {
+            System.out.println("Element found");
+        }
+        else {
+            System.out.println("Element not found");
+        }
     }
 }
